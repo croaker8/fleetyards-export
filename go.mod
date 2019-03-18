816738637888a5e6c5a97aefffd7e6765b2d4541
Matthew Shaver <m@mdsf.us>
2019-03-17 19:27:18 -0700

    Can get ship list

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.12
 
 require (
 	github.com/fleetyards/api-go v0.0.0-20180127162102-b5b3b2b60d97 // indirect
+	github.com/securego/gosec v0.0.0-20190306071850-2bd007e96860 // indirect
 	github.com/tidwall/gjson v1.2.1
 	github.com/tidwall/match v1.0.1 // indirect
 	github.com/tidwall/pretty v0.0.0-20180105212114-65a9db5fad51 // indirect
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -16,7 +17,62 @@ func main() {
 		return
 	}
 
+	getFleet(token)
+
 	signout(token)
+}
+
+func getFleet(token string) {
+
+	modList := make([]string, 0)
+
+	for page := 1; ; page++ {
+
+		url := fmt.Sprintf("https://api.fleetyards.net/v1/vehicles?page=%d", page)
+
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			log.Print("Error creating get vehicles request ", err)
+			return
+		}
+		req.Header.Add("Content-Type", "application/json")
+		req.Header.Add("Authorization", "Bearer "+token)
+		req.Header.Add("Accept", "application/json")
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Fatal("Error sending get vehicles ", err)
+		}
+
+		//log.Print("Status code ", resp.StatusCode)
+
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal("Error reading get vehicles body ", err)
+		}
+
+		bodyString := string(body)
+
+		count := 0
+
+		mods := gjson.Get(bodyString, "#.model.name")
+		mods.ForEach(func(key, value gjson.Result) bool {
+			modList = append(modList, value.String())
+			count++
+			return true // keep iterating
+		})
+
+		if count == 0 {
+			break
+		}
+	}
+
+	log.Printf("Got ship count %d", len(modList))
+	for _, mod := range modList {
+		log.Print(mod)
+	}
+
 }
 
 func signin() (string, error) {
@@ -48,24 +104,23 @@ func signout(token string) {
 	req, err := http.NewRequest("DELETE", "https://api.fleetyards.net/v1/sessions", nil)
 	if err != nil {
 		log.Fatal("Error creating delete request ", err)
-		return
 	}
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("Authorization", "Bearer "+token)
 	req.Header.Add("Accept", "application/json")
 	client := &http.Client{}
-	respd, err := client.Do(req)
+	resp, err := client.Do(req)
 	if err != nil {
 		log.Fatal("Error sending delete ", err)
 	}
 
-	defer respd.Body.Close()
-	body2, err := ioutil.ReadAll(respd.Body)
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal("Error reading delete body ", err)
 	}
 
-	msg := gjson.Get(string(body2), "message")
+	msg := gjson.Get(string(body), "message")
 
 	log.Print("delete msg ", msg.Str)
 
